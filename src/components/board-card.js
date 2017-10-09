0b1413973172d12421d64fa29282c73cff5887b7
TaChyla <jackie@Jackies-MBP.hitronhub.home>
2017-10-09 09:53:38 -0700

    text align center Header

--- src/components/board-card.js
+++ src/components/board-card.js
@@ -6,8 +6,8 @@ class BoardCard extends React.Component {
   render() {
     return (
     <div className="card">
-      <h1>Pinterest</h1>
-      <p>Powered with React</p>
+      <h1 className="pinterestHeader">Pinterest</h1>
+      <p className="pinterestHeader">Powered with React</p>
       <PinsList />
     </div>)
   }
