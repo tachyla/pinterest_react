536cb0712d7ce6bcecfe93696f7c72a67a8bb8aa
Jackie <jackie@Jackies-MBP.hitronhub.home>
2017-10-09 09:42:27 -0700

    flex display on .imageContainer

--- src/components/pins-list.js
+++ src/components/pins-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import pins_formatted from '../pins_formatted.json';
+import '../css/style.css';
 
 
 class PinsList extends React.Component {
@@ -8,7 +9,7 @@ class PinsList extends React.Component {
     const { pinsList } = this.props;
     const pins = pins_formatted.map((pin, index) =>
 
-    <div key={index}>
+    <div className="imageContainer" key={index}>
       <a className="image-link" href={"/pin/" + pin.id} >
         <img src={pin.images.orig.url} alt={pin.description} height="250px" width="250px"/> 
       </a>
@@ -23,3 +24,4 @@ class PinsList extends React.Component {
 }
 
 export default PinsList;
+//2nd commit
