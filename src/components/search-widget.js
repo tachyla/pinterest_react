5a7df802139816290c61f70ca5dade82c698a8e5
TaChyla <jackie@Jackies-MBP.hitronhub.home>
2017-10-10 09:31:02 -0700

    placeholder update

--- src/components/search-widget.js
+++ src/components/search-widget.js
@@ -4,7 +4,7 @@ export default function SearchWidget(props) {
     return (
         <form onSubmit={e => e.preventDefault()}>
             <label htmlFor="search">Filter</label>&emsp;
-            <input type="search" id="filter" name="filter" placeholder="cute" 
+            <input type="search" id="filter" name="filter" placeholder="black" 
                 onChange={e => props.onChange(e.target.value)} />
         </form>
     );
